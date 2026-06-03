e81b9a3f5aeeb52c620273f7bda38882d7299272
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-03 16:29:01 +0530

    Fix voting age condition in else if statement

--- else_if_statement.js
+++ else_if_statement.js
@@ -1,8 +1,23 @@
 let age = 23;
-if(age <=12){
+if(age >=12){
     console.log("you can voyte");
 }
 
-else if(age > 12){
+else if(age <12){
     console.log("you cannot vote");
 }
+
+
+
+
+
+
+
+// let age = 23;
+// if(age <=12){
+    // console.log("you can voyte");
+// }
+
+// else if(age > 12){
+    // console.log("you cannot vote");
+// }
