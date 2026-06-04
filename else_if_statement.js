1932fe2429345064b09b6c50114abf264c09aff2
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-04 16:49:34 +0530

    Refactor: Uncomment age and marks voting logic for clarity

--- else_if_statement.js
+++ else_if_statement.js
@@ -1,53 +1,35 @@
-// let age = 14;
-// if(age >=18){
-    // console.log("you can voyte");
-// }
-// else if(age >=18){
-    // console.log("you can voyte");
-// }
-// else if(age <18){
-    // console.log("you cannot vote2");
-// }
-
-
-
-
-// let marks = 90;
-
-// if(marks >= 89){
-    // console.log("you got A+");
-// }
-// else if (marks >=60){
-    // console.log("you got A");
-// }
-// else if (markes >=40){
-    // console.log("you got B");
-// }
-// else if(marks<40){
-    // coonsole.log("you got F");
-// }
-
-
-
-
-let month ="august";
-
-if (month ==="june"){
-    console.log("summer is here");
-}
-else if(month==="august"){
-    console.log("rainy season is here");
+let age = 14;
+if (age >= 18) {
+  console.log("you can voyte");
+} else if (age >= 18) {
+  console.log("you can voyte");
+} else if (age < 18) {
+  console.log("you cannot vote2");
 }
 
+let marks = 90;
 
+if (marks >= 89) {
+  console.log("you got A+");
+} else if (marks >= 60) {
+  console.log("you got A");
+} else if (markes >= 40) {
+  console.log("you got B");
+} else if (marks < 40) {
+  coonsole.log("you got F");
+}
 
+let month = "august";
 
+if (month === "june") {
+  console.log("summer is here");
+} else if (month === "august") {
+  console.log("rainy season is here");
+}
 
-// let age = 23;
-// if(age <=12){
-    // console.log("you can voyte");
-// }
-
-// else if(age > 12){
-    // console.log("you cannot vote");
-// }
+let age = 23;
+if (age <= 12) {
+  console.log("you can voyte");
+} else if (age > 12) {
+  console.log("you cannot vote");
+}
