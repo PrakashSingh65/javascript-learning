af9d9523bfe10db10098ffc3178e2d9679faba4b
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-04 16:24:08 +0530

    Fix age condition checks for voting eligibility

--- else_if_statement.js
+++ else_if_statement.js
@@ -1,10 +1,12 @@
-let age = 23;
-if(age >=12){
+let age = 14;
+if(age >=18){
     console.log("you can voyte");
 }
-
-else if(age <12){
-    console.log("you cannot vote");
+else if(age >=18){
+    console.log("you can voyte");
+}
+else if(age <18){
+    console.log("you cannot vote2");
 }
 
 
