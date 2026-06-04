53ec4e496f9575533e4e5321931c067097de3bb3
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-04 18:09:37 +0530

    Add popcone price calculation based on size selection

---
+++ practive_qs.js
@@ -0,0 +1,14 @@
+// qs. Create a system to calculate popcone prices based on the size customers asked for:
+
+let size ="small";
+
+if(size ==="small"){
+    console.log("the priice of small popcone is 100");
+}
+else if(size==="medium"){
+    console.log("the price of medium popconne is 150");
+}
+else if(size==="large"){
+    console.log("the price of large popcone is 200");
+
+}
