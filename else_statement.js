2a6cc5f5acb467f41bb88803cd20ffd4876ed7a9
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-04 17:39:12 +0530

    Add initial voting and traffic light logic implementation

---
+++ else_statement.js
@@ -0,0 +1,23 @@
+let age =16;
+if(age>=18){
+    console.log("you can vote");
+}
+else{
+    console.log("you cannot vote");
+}
+
+
+
+
+
+let  color ="black ";
+
+if(color === "red"){
+    console.log("stop");
+}else if(color==="yellow"){
+    console.log("get ready");
+}else if(color==="green"){
+    console.log("go");
+}else{
+    console.log("traffic light is broken");
+}
