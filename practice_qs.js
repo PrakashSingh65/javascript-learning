4659c6ed1a7227da3557e217d125538ee11db531
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-05 22:32:38 +0530

    Add program to check if a string is "good" based on criteria

---
+++ practice_qs.js
@@ -0,0 +1,10 @@
+// Os. A"good string" is a string that starts with  the letter 'a' & has a length > 3. Write a program to find if a string is good or not.
+
+let str ="apple";
+
+if(str[0] === 'a' && str.length >3){
+    console.log("string is good");
+}
+else{
+    console.log("string is not good");
+}
