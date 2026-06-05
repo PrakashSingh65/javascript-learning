6727772796ca66e992aa212f25c0d274e335881e
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-05 17:09:08 +0530

    Add logical operators examples and pass condition check

---
+++ logical_operators.js
@@ -0,0 +1,36 @@
+//Logical Operators
+
+// let marks= 75;
+
+// if(marks>=33 && marks >=80){    # (&&) and operator
+    // console.log("pass");
+    // console.log("A+");
+// }
+
+
+
+
+// let marks= 75;
+
+//  if(marks>=33 || marks >=80){    # (||) or operator
+    // console.log("pass");
+    // console.log("A+");
+// }
+
+
+
+// let marks=75;
+
+// if(!(marks<33)){   # (!) not operator
+    // console.log("pass");
+    // console.log("A+");
+// }
+
+
+
+
+let marks=23;
+
+if((marks>33 && marks<80) || !false){
+    console.log("pass"); //pass
+}
