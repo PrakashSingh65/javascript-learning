30d50d56dadd84cf6c5cb48e4d9b6836a181180b
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-05 16:38:12 +0530

    Add nested if-else structure for marks grading logic

---
+++ nested_if-else.js
@@ -0,0 +1,14 @@
+let marks =30;
+
+if (marks>=33){
+    console.log("you are pass");
+    if(marks>=80){
+        console.log("grade : o");
+    }
+    else{
+        console.log("grade: A");
+    }
+}
+else {
+    console.log("better luck net time ");
+}
