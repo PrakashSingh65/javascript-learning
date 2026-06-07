436be2f229bccace1440591710177a3bfaab08e4
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-07 17:58:37 +0530

    Fix typos in switch statement cases and improve color handling

---
+++ switch_statement.js
@@ -0,0 +1,19 @@
+let color ="black";
+
+
+switch(color){
+    case"red":
+    console.log("stop");
+    break;
+
+    case"yelloe":
+    console.log("get ready");
+    break;
+
+    case"green":
+    comnsole.log("go");
+    brewak;
+
+    default:
+        console.log("invalid color");
+}
