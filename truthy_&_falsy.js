227be363f0623020e34a8dd7912e0ffc8bbb96ee
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-07 17:43:36 +0530

    Refactor truthy and falsy value example by commenting out unused code

--- truthy_&_falsy.js
+++ truthy_&_falsy.js
@@ -1,6 +1,17 @@
-if ("prakash"){
-    console.log("it is true vale");
+// if ("prakash"){
+    // console.log("it is true vale");
+// }
+// else{
+    // console.log("it is false value");
+// }
+
+
+
+let string ="prakash ";
+
+if(string){
+    console.log("string is not empty");
 }
 else{
-    console.log("it is false value");
+    console.log("string is empty");
 }
