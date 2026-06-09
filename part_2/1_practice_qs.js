4a28eb3b31113875e142ae8d1ab327484869e968
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-09 19:43:22 +0530

    Add initial implementation for message trimming and uppercase conversion

---
+++ part_2/1_practice_qs.js
@@ -0,0 +1,11 @@
+// Qs. For the give string:
+// let msg= "help!";
+// Trim it & convert it to uppercase.
+
+
+let msg ="hello";
+console.log(msg.trim().toUpperCase());
+
+
+// let msg ="hello";
+// console.log(msg.trim().toLowerCase());
