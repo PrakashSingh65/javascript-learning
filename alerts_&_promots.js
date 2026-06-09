2ed2881a8efe42d37d7180528a4e2f39708980bb
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-09 11:59:39 +0530

    Comment out logging statements and add prompt for user input

--- alerts_&_promots.js
+++ alerts_&_promots.js
@@ -1,3 +1,7 @@
-console.error("this is an error");
-console.warn("this is a warning");
-console.log("this is a log message");
+// console.error("this is an error");
+// console.warn("this is a warning");
+// console.log("this is a log message");
+
+
+let firstName =prompt("enter your name:");
+console.log ("firstName");
