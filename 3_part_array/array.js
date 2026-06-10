843dc212b87f38449e908ae22acbcc6ee79342e6
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-10 13:50:42 +0530

    Comment out student variable declarations and retain marks_class_12 logging

--- 3_part_array/array.js
+++ 3_part_array/array.js
@@ -2,5 +2,16 @@
 // let students ="prakash";
 // let students ="nakul";
 
-let students =['sandeep','prakash','nakul']
-console.log(students);
+// let students =['sandeep','prakash','nakul']
+// console.log(students);
+
+
+let marks_class_12 =[55,66,22,78,99,false, "not present"]
+console.log(marks_class_12)
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6])
