5dbbdf80dab2b0dd036557dfdec11fa47a0ce822
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-12 23:17:01 +0530

    Refactor marks_class_12 logging and update values

--- 3_part_array/array.js
+++ 3_part_array/array.js
@@ -7,7 +7,6 @@
 
 
 let marks_class_12 =[55,66,22,78,99,false, "not present"]
-console.log(marks_class_12)
 console.log(marks_class_12[0])
 console.log(marks_class_12[1])
 console.log(marks_class_12[2])
@@ -16,3 +15,6 @@ console.log(marks_class_12[4])
 console.log(marks_class_12[5])
 console.log(marks_class_12[6])  
 console.log("the length of marks_class_12 is", marks_class_12.length)
+marks_class_12[6]=89
+marks_class_12[0]=97
+console.log(marks_class_12)
