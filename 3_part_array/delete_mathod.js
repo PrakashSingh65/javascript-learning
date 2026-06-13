82355e0e7a19d50e6d2bad80632d880ebe834c9e
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-13 23:34:49 +0530

    Refactor delete method example and add concatenation of two arrays with console output

--- 3_part_array/delete_mathod.js
+++ 3_part_array/delete_mathod.js
@@ -1,3 +1,8 @@
-let num =[,2,3,4,5,6,7,8]
-delete num[0]
-console.log(num)
+let num =[1,2,3,4,5,6,7,8]
+let num_more =[11,12,13,14,15,16,17,18]
+// delete num[0]
+// console.log(num.length)
+
+let newArray= num.concat(num_more)
+console.log(newArray)
+console.log(num,num_more)
