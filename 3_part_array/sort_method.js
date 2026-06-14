958ac6be635f06d633f5fb9b3437c1986296efe3
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-14 23:12:49 +0530

    Refactor sorting example by commenting out old code and updating array with splice method

--- 3_part_array/sort_method.js
+++ 3_part_array/sort_method.js
@@ -1,6 +1,11 @@
-let compare =(a,b)=>{
-    return a - b
-}
-let num=[552,33,5,457,234,456,678,432,789]
-num.sort(compare)
-console.log(num) 
+// let compare =(a,b)=>{
+    // return b - a
+// }
+// let num=[552,33,5,457,234,456,678,432,789]
+// num.sort(compare)
+// /num.reverse()
+// console.log(num)
+
+let num = [551,22,33,3,14,5,7,8,225]
+num.splice(2,3,1042,1033,1055)
+console.log(num)
