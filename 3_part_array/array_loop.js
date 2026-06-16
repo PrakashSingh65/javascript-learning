3834c9d8de44d06b631cfe204a73cacbe457ee9d
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-16 23:44:53 +0530

    Comment out the traditional for loop and maintain the forEach implementation for squaring array elements

--- 3_part_array/array_loop.js
+++ 3_part_array/array_loop.js
@@ -1,5 +1,10 @@
 let num =[3,54,1,2,4]
 
-for(let i=0; i<num.length;i++){
-    console.log(num[i])
-}
+// for(let i=0; i<num.length;i++){
+    // console.log(num[i])
+// }
+
+
+num.forEach((element)=>{
+    console.log(element*element)
+})
