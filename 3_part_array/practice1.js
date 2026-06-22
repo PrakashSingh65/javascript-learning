eb1c099e27eb30b96bc8d2e7586a34270023745d
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-22 23:51:59 +0530

    Add array manipulation example using splice for clarity

---
+++ 3_part_array/practice1.js
@@ -0,0 +1,7 @@
+// Qs. For the given start of an array, change it to final form usinng splice.
+// start : ['january','july','march','august']
+// final: ['july','june','march','august']
+
+
+let months = ["january","july","march","august"]
+console.log(months.splice(0,2,"july","june"))
