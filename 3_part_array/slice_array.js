cb21fa47063fa1031d54ef7d895dd9ffcbc45531
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-22 17:00:17 +0530

    Update array manipulation example to use splice instead of slice

--- 3_part_array/slice_array.js
+++ 3_part_array/slice_array.js
@@ -1,2 +1,8 @@
+// let cars = ['audi','bmw','xuv','maruti']
+// console.log(cars.slice(1))
+
+
+// splice Array
+
 let cars = ['audi','bmw','xuv','maruti']
-console.log(cars.slice(1))
+console.log(cars.splice(3))
