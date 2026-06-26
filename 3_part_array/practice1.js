524b2271dcadaa4a2b746e4e438c6473a0c0d0de
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-26 10:19:04 +0530

    Comment out unused code examples for clarity and consistency

--- 3_part_array/practice1.js
+++ 3_part_array/practice1.js
@@ -3,13 +3,21 @@
 // final: ['july','june','march','august']
 
 
-let months = ["january","july","march","august"]
-console.log(months.splice(0,2,"july","june"))
+// let months = ["january","july","march","august"]
+// console.log(months.splice(0,2,"july","june"))
 
 
 // Qs. Return the index of the "javascript" from the given array,if it was reversed.
 // ['c','c++','html','javascript','python','java','c#','sql']
 
 
+// let lang = ["c","c++","html","javascript","python","java","c#"];
+// console.log(lang.push("sql"))
+
+
+// let lang = ["c","c++","html","javascript","python","java","c#"];
+// console.log(lang.reverse())
+
+
 let lang = ["c","c++","html","javascript","python","java","c#"];
-console.log(lang.push("sql"))
+console.log(lang.reverse().indexOf("javascipt"))
