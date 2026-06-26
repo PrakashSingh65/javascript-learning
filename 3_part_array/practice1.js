ef56d683043fb76a5cc5688777326a233060abfb
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-26 10:13:30 +0530

    Add additional array manipulation example and comments for clarity

--- 3_part_array/practice1.js
+++ 3_part_array/practice1.js
@@ -5,3 +5,11 @@
 
 let months = ["january","july","march","august"]
 console.log(months.splice(0,2,"july","june"))
+
+
+// Qs. Return the index of the "javascript" from the given array,if it was reversed.
+// ['c','c++','html','javascript','python','java','c#','sql']
+
+
+let lang = ["c","c++","html","javascript","python","java","c#"];
+console.log(lang.push("sql"))
