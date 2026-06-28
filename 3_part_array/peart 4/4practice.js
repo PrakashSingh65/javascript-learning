e02c1a87d7a74fafb1675be4b11030ea209e959d
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-29 00:37:02 +0530

    Fix prompt logic for favorite movie guessing game

--- 3_part_array/peart 4/4practice.js
+++ 3_part_array/peart 4/4practice.js
@@ -7,3 +7,10 @@ let guess = peompt("guess my favorit movie");
 while( (guess !=favMovie) && (guess != "quite")){
     guess = prompt("wrong guess. Please try again");
 }
+
+
+if(guess == favMovie){
+    console.log("congrats");
+}else{
+    console.log("quit")
+}
