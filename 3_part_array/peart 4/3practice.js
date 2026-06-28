7b63f847d0fbf8ad5dd04638b35de2dc8c2018b4
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-28 23:48:06 +0530

    Refactor multiplication table script for clarity and maintainability

--- 3_part_array/peart 4/3practice.js
+++ 3_part_array/peart 4/3practice.js
@@ -1,5 +1,12 @@
 // Print the multiplication table for 5
 
-for(let i=5; i<=50; i=i+5){
-    console.log(i);
-}
+
+// let n = prompt("write your number");
+// n = parseInt(n);
+// for(let i=n; i<=n*10; i=i+n){
+    // console.log(i);
+// }
+
+ for(let i=5; i<=50; i=i+5){
+     console.log(i);
+ }
