7a7e2ab6104fa3037c473a2d9e6fe1da50b113d5
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-29 00:32:18 +0530

    Add script to prompt user for favorite movie guess

---
+++ 3_part_array/peart 4/4practice.js
@@ -0,0 +1,9 @@
+// crating favorite movie
+
+const fevMovie ="avatar";
+
+let guess = peompt("guess my favorit movie");
+
+while( (guess !=favMovie) && (guess != "quite")){
+    guess = prompt("wrong guess. Please try again");
+}
