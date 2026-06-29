fd00bcbd6960859c5231b8f332fc3597cd65b5bc
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-29 13:35:18 +0530

    Update loops_array.js to log all fruits instead of every second fruit

--- 3_part_array/peart 4/loops_array.js
+++ 3_part_array/peart 4/loops_array.js
@@ -6,8 +6,16 @@
 
 
 
+// let fruits =[ "mango","apple","orange","banana","litchi"];
+
+// for(let i=1; i<fruits.length; i=i+2){
+//     console.log(i, fruits[i]);
+// }
+
+
+
 let fruits =[ "mango","apple","orange","banana","litchi"];
 
-for(let i=1; i<fruits.length; i=i+2){
+for(let i=0; i<fruits.length; i++){
     console.log(i, fruits[i]);
 }
