33fb8ba1fc55aea3e9e7b813ab6c32b905c180c3
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-29 15:48:14 +0530

    Comment out unused heroes loop and related code in nested_loop.js

--- 3_part_array/peart 4/nested_loop.js
+++ 3_part_array/peart 4/nested_loop.js
@@ -6,12 +6,24 @@
 // }
 
 
-let heroes = [ ["ironman","spiderman","thor"],["superman","wonder woman","flash"]];
+// let heroes = [ ["ironman","spiderman","thor"],["superman","wonder woman","flash"]];
 
-for(let i=0; i<heroes.length; i++){
-    console.log('list #${i}');
-    for(let j=0; j<heroes[i].length; j++){
-        console.log(heroes[i][j]);
+// for(let i=0; i<heroes.length; i++){
+//     console.log('list #${i}');
+//     for(let j=0; j<heroes[i].length; j++){
+//         console.log(heroes[i][j]);
+//     }
+// }
+
+
+
+
+let student = [ ["prakash",95],["pratap",99],["sandeep",98],["nakul",80.6]];
+
+
+for(let i=0; i<student.length; i++){
+    for(let j=0; j<student[i].length; j++){
+        console.log(student[i][j]);
     }
 }
 
