d9bf48faad112f3a1887134b3db144ee0af71a5f
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-29 16:08:50 +0530

    Comment out unused fruit and character loops in for_of_loop.js

--- 3_part_array/peart 4/for_of_loop.js
+++ 3_part_array/peart 4/for_of_loop.js
@@ -1,9 +1,22 @@
-let fruits = ["mango","apple","banana","orange"];
+// let fruits = ["mango","apple","banana","orange"];
 
-for(fruit of fruits){
-    console.log(fruit);
+// for(fruit of fruits){
+//     console.log(fruit);
+// }
+
+// for(char of "prakash"){
+//     console.log(char);
+// }
+
+
+// nested for-loop
+
+let heroes = [ ["superman","batman","wonder woman"],["spiderman","ironman","thor"]];
+
+for(list of heroes){
+    console.log(list);
+    for(hero of list){
+        console.log(hero);
+    }
 }
 
-for(char of "prakash"){
-    console.log(char);
-}
