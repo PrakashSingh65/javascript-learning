2a40ef2cd90049304eb3ed4b53be3386881ab3c3
PrakashSingh65 <pratapsing5656@gmail.com>
2026-06-29 15:41:18 +0530

    Comment out unused outer loop and update nested loop to log heroes

--- 3_part_array/peart 4/nested_loop.js
+++ 3_part_array/peart 4/nested_loop.js
@@ -1,6 +1,17 @@
-for(let i=1; i<=3; i++){
-    console.log('outer loop $(i)');
-    for(let j=1; j<=3; j++){
-        console.log(j);
+// for(let i=1; i<=3; i++){
+//     console.log('outer loop $(i)');
+//     for(let j=1; j<=3; j++){
+//         console.log(j);
+//     }
+// }
+
+
+let heroes = [ ["ironman","spiderman","thor"],["superman","wonder woman","flash"]];
+
+for(let i=0; i<heroes.length; i++){
+    console.log('list #${i}');
+    for(let j=0; j<heroes[i].length; j++){
+        console.log(heroes[i][j]);
     }
 }
+
