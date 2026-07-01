6fa9d405525e5edfb896c9ab99f80a74ddd49258
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-01 23:20:36 +0530

    Comment out print1to100 function and its invocation in functions.js

--- part 5/functions.js
+++ part 5/functions.js
@@ -21,10 +21,26 @@
 
 
 
-function print1to100() {
-    for(let i=1; i<=100; i++) {
-        console.log(i);
+// function print1to100() {
+//     for(let i=1; i<=100; i++) {
+//         console.log(i);
+//     }
+// }
+
+// print1to100();
+
+
+
+
+
+function isAdult(){
+    let age =18;
+    if(age>=18){
+        console.log("you are adult");
+    }else{
+        console.log("you are not adult");
     }
 }
 
-print1to100();
+
+isAdult();
