9d5d2e6b4bc5020b384fcfe2761f4bfb04bec063
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-01 23:25:56 +0530

    Add printPoem function to output a poem

---
+++ part 5/1pratice.js
@@ -0,0 +1,10 @@
+// create a function that print a poem
+
+function printPoem(){
+    console.log("Roses are red,");
+    console.log("Violets are blue,");
+    console.log("Sugar is sweet,");
+    console.log("And so are you.");
+}
+
+printPoem();
