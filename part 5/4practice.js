53ddb604eedac475be7b5614d9e803d9ab631b8a
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-03 02:50:57 +0530

    Add function to print the multiplication table of a number

---
+++ part 5/4practice.js
@@ -0,0 +1,10 @@
+// create a function that prints the multiplication table of a number
+
+
+function printTable(n){
+    for(let i= n; i<=n*10; i+=10){
+        console.log(i);
+    }
+}
+
+printTable(5);
