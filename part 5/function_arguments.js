2c62426512e18805f09800dafee34873cdd19bd4
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-02 15:44:18 +0530

    Comment out printInfo function and its call; retain sum function for addition operations

--- part 5/function_arguments.js
+++ part 5/function_arguments.js
@@ -5,8 +5,18 @@
 // printName("prakash");
 
 
-function printInfo(name,age){
-    console.log(`${name}'s age is ${age}.`);
+// function printInfo(name,age){
+//     console.log(`${name}'s age is ${age}.`);
+// }
+
+// printInfo("prakash",27);
+
+
+
+function sum(a,b){
+    console.log(a+b);
 }
 
-printInfo("prakash",27);
+sum(10,30);
+sum(20,70);
+sum(50,80);
