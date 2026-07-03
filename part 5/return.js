6c012101dd06e05a2eedd94377d5b8b45a1591a6
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-03 23:12:37 +0530

    Remove commented-out code and clean up sum function usage

--- part 5/return.js
+++ part 5/return.js
@@ -1,5 +1,13 @@
+// function sum (a,b){
+//     return a+b;
+// }
+// let s = sum(3,4);
+// console.log(s);
+
+
+
 function sum (a,b){
     return a+b;
 }
-let s = sum(3,4);
-console.log(s);
+
+console.log(sum(3,4));
