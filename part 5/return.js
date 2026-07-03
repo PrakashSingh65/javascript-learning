0bae201642352024b60f166d132d5761796dc80e
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-03 23:15:10 +0530

    Refactor sum function and remove commented-out code

--- part 5/return.js
+++ part 5/return.js
@@ -6,8 +6,16 @@
 
 
 
-function sum (a,b){
+// function sum (a,b){
+//     return a+b;
+// }
+
+// console.log(sum(3,4));
+
+
+
+function sum(a,b){
     return a+b;
 }
 
-console.log(sum(3,4));
+console.log(sum(sum(1,2),3));
