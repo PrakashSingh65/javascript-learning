bb496db5e987550ac11fa3eb0d95a1cdcfea7363
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-04 12:31:42 +0530

    Remove commented-out code for calSum function and its invocation

--- part 5/scope.js
+++ part 5/scope.js
@@ -1,3 +1,15 @@
+// function calSum(a,b){
+//     let sum = a+b;
+//     console.log(sum);
+// }
+
+// calSum(1,2);
+
+
+
+let sum =56;
+
+
 function calSum(a,b){
     let sum = a+b;
     console.log(sum);
