b49a76243cb02df47d81e2bc36b560a0fd7143ec
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-05 17:03:23 +0530

    Add spacing for readability in changeGreet function

--- part 5/7practice.js
+++ part 5/7practice.js
@@ -4,9 +4,12 @@ let greet = "hello";
 function changeGreet(){
     let greet ="namaste";
     console.log(greet);
+
+
     function innerGreet(){
         console.log(greet);
     }
+    innerGreet();
 }
 
 console.log(greet);
