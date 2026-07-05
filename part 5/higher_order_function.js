9495bf105aa04c187d40a83da02bad5a2735b3cc
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-05 18:18:24 +0530

    Refactor higher_order_function.js by commenting out multipleGreet function and adding oddOrEvenFactory for odd number detection

--- part 5/higher_order_function.js
+++ part 5/higher_order_function.js
@@ -1,11 +1,41 @@
-function multipleGreet(func, count){
-    for(let i=1; i<=count; i++){
-        func();
+// function multipleGreet(func, count){
+//     for(let i=1; i<=count; i++){
+//         func();
+//     }
+// }
+
+// let greet = function(){
+//     console.log("hello");
+// }
+
+// multipleGreet(greet, 2);
+
+
+
+// highter order function(returns)
+
+
+
+
+let odd = function (n){
+    console.log (!(n%2==0));
+}
+
+let even = function(n){
+    console.log(n%2==0);
+}
+
+
+function oddOrEvenFactorey(request){
+    if(request == "odd"){
+        let odd = function(n){
+            console.log(!(n%2 == 0));
+        }
+        return odd;
     }
 }
 
-let greet = function(){
-    console.log("hello");
-}
 
-multipleGreet(greet, 2);
+let request = "odd";
+let oddFunc = oddOrEvenFactorey(request);
+oddFunc(5);
