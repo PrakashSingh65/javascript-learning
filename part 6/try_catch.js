014e6083d07f0b611675760d1a25cd16000211b6
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-05 23:38:34 +0530

    Add try-catch block for error handling and log messages

---
+++ part 6/try_catch.js
@@ -0,0 +1,10 @@
+console.logf("hello");
+console.log("hello");
+try{
+    console.log("caught an error.. a is not defined");
+}catch{
+    console.log("caught an error... a is not defined");
+}
+
+console.log("hello2");
+console.log("hello2");
