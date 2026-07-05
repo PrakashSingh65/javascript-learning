4111dee070e709c430a7a155ecc423944e2da23d
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-05 18:26:20 +0530

    Add calculator object with basic arithmetic functions

---
+++ part 5/methods.js
@@ -0,0 +1,14 @@
+const calculator={
+    add: function(a, b){
+        return a+b;
+    },
+    sub: function(a ,b){
+        return a-b;
+    },
+    mul: function(a,b){
+        return a*b;
+    },
+    dev: function(a,b){
+        return a/b;
+    }
+}
