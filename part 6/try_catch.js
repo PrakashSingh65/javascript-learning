c517f5a48b272dfc01db927b6e945f96740362f6
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-06 23:26:54 +0530

    Refactor try-catch block to comment out unused code and maintain console logs

--- part 6/try_catch.js
+++ part 6/try_catch.js
@@ -1,11 +1,27 @@
-console.logf("hello");
-console.log("hello");
-try{
-    console.log("a");
-}catch(err){
-    console.log("caught an error... a is not defined");
-    console.log(err);
-}
+// console.logf("hello");
+// console.log("hello");
+// try{
+//     console.log("a");
+// }catch(err){
+//     console.log("caught an error... a is not defined");
+//     console.log(err);
+// }
 
-console.log("hello2");
-console.log("hello2");
+// console.log("hello2");
+// console.log("hello2");
+
+
+
+
+
+
+// set time out 
+
+
+console.log("hi there!");
+
+setTimeout( ()=>{
+    console.log("prakash");
+},400);
+
+console.log("welcome to");
