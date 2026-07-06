a5a00afe8b059cef5f1cf6732f9bdeaf89384d26
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-06 23:52:24 +0530

    Comment out square function and its console log for clarity

--- part 6/1practice.js
+++ part 6/1practice.js
@@ -1,6 +1,20 @@
 // write an arrow function that return  the square of a number 'n'
 
 
-const square = (n) =>n*n;
+// const square = (n) =>n*n;
 
-console.log(square(5));
+// console.log(square(5));
+
+
+
+
+// Qs2.  Write a function that print "hello world" 5 times at intervals of 2s each
+
+let id = setInterval(() => {
+    console.log("hello world");
+},2000);
+
+
+setTimeout(()=>{
+    clearInterval(id);
+},10000);
