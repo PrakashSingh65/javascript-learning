bb109bf41554b5c7f27a06b35939c57efe26d538
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-07 23:29:56 +0530

    Comment out unused variables and functions for clarity

--- part 5/function_expression.js
+++ part 5/function_expression.js
@@ -1,7 +1,16 @@
-let name ="prakaash";
+// let name ="prakaash";
 
-let sum = function(a, b){
-    return a + b;
-}
+// let sum = function(a, b){
+//     return a + b;
+// }
 
-console.log(sum(5, 10));
+// console.log(sum(5, 10));
+
+
+
+let num =[1,2,3,4,5,6,7,8,9];
+
+let double =num.map((el) =>{
+    return el* el;
+});
+console.log(double);
