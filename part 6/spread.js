ec303157f15c59c6bb26b63097e5045013b36294
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-08 16:21:49 +0530

    Add spread operator example with object copying

---
+++ part 6/spread.js
@@ -0,0 +1,15 @@
+// let arr =[1,2,4,5,6,7,8,9];
+
+// let newArr =[...arr];
+// console.log(newArr);
+
+
+const data ={
+    email: "prakash123@gmail.com",
+    pasword: "abcd",
+};
+
+
+const dataCopy= {...data, id:123, country: "india"};
+
+console.log(data);
