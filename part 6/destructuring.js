52cb0cbb581caf4a92e0fd0f848abe0f0e955226
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-08 17:04:21 +0530

    Add example of object destructuring with student data

---
+++ part 6/destructuring.js
@@ -0,0 +1,22 @@
+// let names = ["prakash","sandeep","nakul","tony"];
+
+// let [winner,runnerup, ...other]=names;
+// console.log(winner,runnerup,other);
+
+
+
+// destructuring(object)
+
+
+const student ={
+    name: "prakash",
+    age:23,
+    class: 8,
+    subject:["hindi","english","maths","scienmce"],
+    username: "prakash@123",
+    pasword:"abcd"
+};
+
+
+let username = student.username;
+let password = student.password;
