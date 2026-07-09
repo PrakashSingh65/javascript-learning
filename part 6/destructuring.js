2cfd1ea92651efa304adbf27c7cbb370a36afbd8
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-09 23:28:42 +0530

    Comment out query selector console logs for cleaner output

--- part 6/destructuring.js
+++ part 6/destructuring.js
@@ -40,8 +40,11 @@
 // query selectors
 
 
-console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("p"));
 
-console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector("#description"));
 
-console.dir(document.querySelector(".oldImage"));
+// console.dir(document.querySelector(".oldImage"));
+
+
+console.dir(document.querySelector("div a"));
