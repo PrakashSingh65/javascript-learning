14fab0bad46caf97ab403a16a953f6f93b1c30e0
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-13 00:38:47 +0530

    Refactor HTML structure and comment out unused JavaScript event listeners

--- part 6/event.js
+++ part 6/event.js
@@ -1,11 +1,18 @@
-let p  = document.querySelector("p");
+// let p  = document.querySelector("p");
 
-p.addEventListener("click", function(){
-    console.log("parah was clicked");
-});
+// p.addEventListener("click", function(){
+//     console.log("parah was clicked");
+// });
 
 
-let box = document.querySelector(".box");
-box.addEventListener("mouseenter", function(){
-    console.log("mouse inside box");
+// let box = document.querySelector(".box");
+// box.addEventListener("mouseenter", function(){
+//     console.log("mouse inside box");
+// });
+
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+    console.log(this);
 });
