fd4f3b86f73713408acc9b4bca6b01cf985d6c23
PrakashSingh65 <pratapsing5656@gmail.com>
2026-07-13 02:12:33 +0530

    Add variable declarations for paragraph and headings in event.js

--- part 6/event.js
+++ part 6/event.js
@@ -12,6 +12,9 @@
 
 
 let btn = document.querySelector("button");
+let p = document.querySelector("p");
+let h1 = document.querySelector("h1");
+let h3 = document.querySelector("h3")
 
 btn.addEventListener("click", function(){
     console.dir(this.innerText);
